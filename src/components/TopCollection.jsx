b358d93b12c138c2231dac8cbe51682e7d1d878a
BerserkZ <zakizaka74@gmail.com>
2025-08-17 17:24:37 +0100

    deleting a file

--- src/components/TopCollection.jsx
+++ src/components/TopCollection.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import styled from "styled-components";
-import fetchingData from "./FetchingData";
+//import fetchingData from "./FetchingData";
 import { useEffect, useState } from "react";
 import { loadCollections } from "./moralis/moralisService.js";
 import { openSea } from "./moralis/openSea.js";
@@ -8,7 +8,7 @@ import { openSea } from "./moralis/openSea.js";
 const SectionContainer = styled.div`
   width: 100%;
   height: 600px;
-  //border: white solid 1px;
+  
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -41,7 +41,7 @@ const ContentContainer = styled.section`
   left: 0px;
   width: 100%;
   height: 100%;
-  //border: 1px solid white;
+  
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -50,7 +50,7 @@ const ContentContainer = styled.section`
 const CollectionTimeHolder = styled.section`
   width: 100%;
   height: 100px;
-  //border: 1px solid black;
+ 
   display: flex;
   justify-content: space-between;
   align-items: center;
